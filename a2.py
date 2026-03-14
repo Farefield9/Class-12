f07b6e712ff217bc9959a08374546ecce809e601
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-03-14 23:43:46 +0530

    Create a2.py

---
+++ a2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+a = pd.DataFrame([['Divya','HR',95000],['Mamta','Marketing',97000],['Payal','IT',980000],['Deepak','Sales',79000]],
+                 columns = ['name','dept','sal'])
+print(a)
