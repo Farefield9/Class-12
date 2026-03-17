c663e129675b977f16224a48c779ed745d246736
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-03-17 18:40:45 +0530

    Create a5.py

---
+++ a5.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+a = [145,141,142,142,143,144,141,140,143,144]
+pl.hist(a, bins = [140,142,144,146])
+pl.show()
+#pl.savefig('d:\\a.jpg')
