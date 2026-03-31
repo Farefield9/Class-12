3141197e9b5e52cf00013414b88bcfa8901f0c88
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-03-31 13:57:15 +0530

    Create a16.py

---
+++ a16.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+a =['s','g','b','d']
+b = [25,31,29,28]
+pl.plot(a,b, color = 'red', marker = '*')
+pl.figure(figsize = (5,4))
+pl.show()
