1371437dd51633853bc4e4fe4de2e98d481a2cfa
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-04-10 18:48:37 +0530

    Create a26.py

---
+++ a26.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+l = [1,4]
+print(l*2)
+pl.hist([1,2,11,12,21,22,23,24,25], bins = [0,10,20,30])
+pl.yticks(range(1,6))
+pl.show()
