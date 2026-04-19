0d7eed46628e42e715444c7d7adb1745f4ee1f54
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-04-19 13:07:07 +0530

    Update a34.py

--- a34.py
+++ a34.py
@@ -1,1 +1,13 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+eng = [56,78,90,34]
+sci = [65,77,54,32]
+maths = [45,67,43,41]
+a = np.arange(4)
+pl.bar(a,eng,width = 0.25)
+pl.bar(a+0.25,sci,width = 0.25)
+pl.title('Subject Analysis')
+pl.xlabel('Marks')
+pl.ylabel('Subjects')
+pl.show()
