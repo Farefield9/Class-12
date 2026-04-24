dad46d071515374cec520e76b227034c66fdd25e
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-04-24 17:06:54 +0530

    Update a38.py

--- a38.py
+++ a38.py
@@ -1,1 +1,6 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+dic = {"Model": ["Samurai", "Accord", "CR-V", "Nexon"],"Brand": ["Suzuki", "Honda", "Honda", "Tata"],"Make": [1993, 1997, 1997, 2021]}
+car = pd.Series(dic)
+print(car)
